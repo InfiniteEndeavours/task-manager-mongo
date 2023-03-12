0fbc60f27bbf8a0551e12a69b92f4ed04fb051d8
Ethan Pitt <EthanPitt01@protonmail.com>
2023-03-12 18:19:53 +0000

    Bind data from DB to accordion

--- static/js/script.js
+++ static/js/script.js
@@ -3,4 +3,6 @@
     M.Sidenav.init(sideNav, {edge: "right"});
     let accordian = document.querySelectorAll('.collapsible');
     M.Collapsible.init(accordian);
+    let tooltips = document.querySelectorAll('.tooltipped');
+    M.Tooltip.init(tooltips);
   });
