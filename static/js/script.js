75b3de75608edea631eb98dde555afafb700e743
Ethan Pitt <EthanPitt01@protonmail.com>
2023-03-12 19:17:29 +0000

    Add categories dropdown to add_task

--- static/js/script.js
+++ static/js/script.js
@@ -1,5 +1,5 @@
-  document.addEventListener('DOMContentLoaded', function() {
-    let sideNav= document.querySelectorAll('.sidenav');
+document.addEventListener('DOMContentLoaded', function () {
+    let sideNav = document.querySelectorAll('.sidenav');
     M.Sidenav.init(sideNav, {edge: "right"});
     let accordian = document.querySelectorAll('.collapsible');
     M.Collapsible.init(accordian);
@@ -7,11 +7,14 @@
     M.Tooltip.init(tooltips);
     let datepicker = document.querySelectorAll('.datepicker');
     M.Datepicker.init(datepicker,
-        {format: "dd mmmm, yyyy",
-        yearRange: 3,
-        showClearBtn: true,
-        i18n: {
-            done: "Select"
-        }
+        {
+            format: "dd mmmm, yyyy",
+            yearRange: 3,
+            showClearBtn: true,
+            i18n: {
+                done: "Select"
+            }
         });
-  });
+    let selection = document.querySelectorAll('select');
+    M.FormSelect.init(selection);
+});
