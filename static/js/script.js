d889af2e594e1c50b734233d625005b3d0dfd428
Ethan Pitt <EthanPitt01@protonmail.com>
2023-03-12 16:43:37 +0000

    Add task accordions

--- static/js/script.js
+++ static/js/script.js
@@ -1,4 +1,6 @@
   document.addEventListener('DOMContentLoaded', function() {
     let sideNav= document.querySelectorAll('.sidenav');
     M.Sidenav.init(sideNav, {edge: "right"});
+    let accordian = document.querySelectorAll('.collapsible');
+    M.Collapsible.init(accordian);
   });
